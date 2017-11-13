26256d69adcc6611218f9847267c0aade216922d
Tim Allen <tim@noblesamurai.com>
2017-11-13 11:23:15 +1100

    remove test/index

--- test/index.js
+++
@@ -1,8 +0,0 @@
-const expect = require('chai').expect;
-
-describe('my thing', function () {
-  it('should work', function () {
-    expect(true).to.be.true;
-    throw new Error('unimplemented');
-  });
-});
